10b0920ec32cd32253ac991a2fa19261c3f0d5f3
sergio arnese <sergio.arnese@gmail.com>
2021-01-25 18:25:04 +0100

    little refactoring on class state

--- src/main/java/it/sergio/arnese/pair/GooseGame.java
+++ src/main/java/it/sergio/arnese/pair/GooseGame.java
@@ -3,12 +3,12 @@ package it.sergio.arnese.pair;
 import java.util.*;
 
 public class GooseGame {
-    private Map<String, Integer> playersPosition = new HashMap<>();
+    private final Map<String, Integer> playersPosition = new HashMap<>();
 
     public void addPlayer(String player) {
         Objects.requireNonNull(player);
 
-        if( this.playersPosition.keySet().contains(player) ) {
+        if( this.playersPosition.containsKey(player) ) {
             throw new IllegalArgumentException("there is already a player: " + player);
         }
 
